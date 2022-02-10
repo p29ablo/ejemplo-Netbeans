a09297533be61951485690c47ec67193ece42b79
pablomolina <pablomolina@192.168.204.202>
2022-02-10 10:39:15 -0500

    Operadores de incremento decremento v1

---
+++ src/main/java/ClsHolaMundo1.java
@@ -0,0 +1,45 @@
+
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+public class ClsHolaMundo1 {
+
+    
+    public static void main(String[] args) {
+    
+    
+    //operadores aritmeticos    
+    /*Scanner entrada = new Scanner(System.in);
+    float numero1, numero2, suma, resta, multiplicacion, division, resto;
+    
+    System.out.println("ingrese dos numeros:  ");
+    numero1 = entrada.nextFloat();
+    numero2 = entrada.nextFloat();
+    
+    suma = numero1+numero2;
+    resta = numero1+numero2;
+    multiplicacion = numero1*numero2;
+    division = numero1/numero2;
+    resto = numero1%numero2;
+    
+    System.out.println("la suma es: "+" "+suma);
+    System.out.println("la resta es: "+" "+resta);
+    System.out.println("la multiplicacion es: "+" "+multiplicacion);
+    System.out.println("la division es: "+" "+division);
+    System.out.println("la residuo es: "+" "+resto);*/
+    
+    //operadores de asignacion
+    /*int numero=5;
+    //numero = numero+5;
+    numero+=5;
+    System.out.println(numero);*/
+    
+    int x=5, y=6;
+    x=++x;
+    y=--y;
+    
+    System.out.println(x);
+    System.out.println(y);
+            
+    }
+    
+}
